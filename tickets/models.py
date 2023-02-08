dc600cb5802b052e1d88e9a74ae36f001dfac9cb
Mradula <mradulammittal@gmail.com>
2023-02-08 23:37:14 +0530

    Adding Models

--- tickets/models.py
+++ tickets/models.py
@@ -2,4 +2,16 @@ from django.db import models
 
 # Create your models here.
 class Site(models.Model):
-    pass
+    name = models.CharField(max_length=200)
+    location = models.CharField(max_length=200)
+    state = models.CharField(max_length=50)
+    image = models.ImageField(height_field=150, width_field=150)
+    price = models.IntegerField()
+
+#class Ticket(Site):
+#    site = models.ForeignKey(Site, on_delete=models.CASCADE)
+#    visit_date = models.DateField()
+#    num_people = models.IntegerField()
+#    fair = models.IntegerField()
+
+
